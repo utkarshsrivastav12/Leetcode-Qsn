c258ba8a9e5eb95366186f11cba3d5542e140d81
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-08-06 23:56:42 +0530

    Time: 0 ms (100%), Space: 45.4 MB (25.8%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,14 +2,14 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         // to calculate sum of given number
-        int sumOfn= (int)n*(n+1)/2;
-        int sumEle=0;
+        int expectedSum = (int)n*(n+1)/2;
+        int actualSum=0;
         //for calculating to sum of all element in array
         for(int i = 0 ;i<n;i++){
-            sumEle+=nums[i];
+            actualSum+=nums[i];
         }
         //for find to which element miss in the array
-        int res= sumOfn-sumEle;
+        int res= expectedSum-actualSum;
     return res; 
     }
 }
