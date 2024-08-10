590513825c7e4fdc4f18cef6c9466e7f09406da8
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-08-10 23:43:49 +0530

    Time: 6 ms (85.59%), Space: 43.3 MB (53.68%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,16 +1,45 @@
 class Solution {
     public String reverseWords(String s) {
-    String ansArr[] = s.split(" ");
-    int n = ansArr.length;
-   String ans = "";
+ String Arr[] = s.split(" ");
+    int n = Arr.length;
+   StringBuilder ans = new StringBuilder();
         for(int i =n-1;i>=0;i--){
-            if(!ansArr[i].isEmpty() && ans.isEmpty()){        
-            ans+=ansArr[i];
-            }else if(!ansArr[i].isEmpty() && !ans.isEmpty()){
-                ans+=" "+ansArr[i];
+            if(!Arr[i].isEmpty() && ans.isEmpty()){        
+            ans.append(Arr[i]);
+            }else if(!Arr[i].isEmpty() && !ans.isEmpty()){
+                ans.append(" ");
+                ans.append(Arr[i]);
             }
         }
         
-        return ans;
+        return ans.toString();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     String ansArr[] = s.split(" ");
+//     int n = ansArr.length;
+//    String ans = "";
+//         for(int i =n-1;i>=0;i--){
+//             if(!ansArr[i].isEmpty() && ans.isEmpty()){        
+//             ans+=ansArr[i];
+//             }else if(!ansArr[i].isEmpty() && !ans.isEmpty()){
+//                 ans+=" "+ansArr[i];
+//             }
+//         }
+        
+//         return ans;
+//     }
 }
