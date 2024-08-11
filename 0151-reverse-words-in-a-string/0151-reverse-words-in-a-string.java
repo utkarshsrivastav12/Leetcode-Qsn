f87da42ff64ef79203a6bd478744575ffb47f142
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-08-11 06:44:33 +0530

    Time: 3 ms (98.78%), Space: 42.9 MB (75.91%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,45 +1,43 @@
 class Solution {
-    public String reverseWords(String s) {
-//  String Arr[] = s.split(" ");
-//     int n = Arr.length;
-//    StringBuilder ans = new StringBuilder();
+ public static String reverseWords(String s) {
+        StringBuilder ans = new StringBuilder();
+        int startIndex = s.length()-1;
+        while(startIndex>=0){ 
+            while(startIndex>=0 && s.charAt(startIndex)==' '){
+                startIndex--;
+            }
+            if(startIndex < 0){
+                break;
+            }
+            int endIndex = startIndex;
+            // Find the start of the word
+            while(startIndex >= 0 && s.charAt(startIndex)!= ' '){
+                startIndex--;
+            }
+            if(ans.isEmpty()){        
+            ans.append(s.substring(startIndex+1, endIndex+1));
+            }else if(!ans.isEmpty()){
+                ans.append(" ");
+                ans.append(s.substring(startIndex+1, endIndex+1));
+            }
+        }
+return ans.toString();
+    }
+
+
+
+//1st method
+//     String ansArr[] = s.split(" ");
+//     int n = ansArr.length;
+//    String ans = "";
 //         for(int i =n-1;i>=0;i--){
-//             if(!Arr[i].isEmpty() && ans.isEmpty()){        
-//             ans.append(Arr[i]);
-//             }else if(!Arr[i].isEmpty() && !ans.isEmpty()){
-//                 ans.append(" ");
-//                 ans.append(Arr[i]);
+//             if(!ansArr[i].isEmpty() && ans.isEmpty()){        
+//             ans+=ansArr[i];
+//             }else if(!ansArr[i].isEmpty() && !ans.isEmpty()){
+//                 ans+=" "+ansArr[i];
 //             }
 //         }
         
-//         return ans.toString();
+//         return ans;
 //     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    String ansArr[] = s.split(" ");
-    int n = ansArr.length;
-   String ans = "";
-        for(int i =n-1;i>=0;i--){
-            if(!ansArr[i].isEmpty() && ans.isEmpty()){        
-            ans+=ansArr[i];
-            }else if(!ansArr[i].isEmpty() && !ans.isEmpty()){
-                ans+=" "+ansArr[i];
-            }
-        }
-        
-        return ans;
-    }
 }
