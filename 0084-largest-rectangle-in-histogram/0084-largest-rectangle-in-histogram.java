a24423f35b9a3e9e9aaa05ea371078a0de6aa2f7
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-08-15 03:55:51 +0530

    Time: 71 ms (53.97%), Space: 57.1 MB (73.39%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,49 +1,74 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int ns[] = findNextSmaller(heights);
-        int ps[] = findPrevSmaller(heights);
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<heights.length;i++){
-            int h = heights[i];
-            int w = (ns[i]-ps[i])-1;;
-            int area = h*w;
-            max= Math.max(max,area);
-        }
-    return max;
-    }
-    public int[] findNextSmaller(int[] arr){
-     int n = arr.length;
-     int res[] = new int[n];
-     Stack<Integer>stack= new Stack<>();
-     for(int i = n-1;i>=0;i--){
-        while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
-            stack.pop();
-        }
-        if(stack.isEmpty()){
-            res[i]= n;
-        }else{
-            res[i] = stack.peek();
-        }
-        stack.push(i);
-     }
-     return res;
-    }
+      //using single  stack...
+      Stack<Integer>stack = new Stack<>();
+      int n = heights.length;
+      int max = Integer.MIN_VALUE;
+      for(int i = 0; i<=n;i++){
+        int element =(i==n)?0:heights[i];
+       while(!stack.isEmpty() && heights[stack.peek()]>element){
+        int h =heights[stack.pop()];
+        int ns = i;
+        int ps =(stack.isEmpty())?-1:stack.peek();
+        int w = ns-ps-1;
+        int area = h*w;
+        max = Math.max(max,area);
+       }
+       stack.push(i);
 
-    public int[] findPrevSmaller(int[] arr){
-     int n = arr.length;
-     int res[] = new int[n];
-     Stack<Integer>stack= new Stack<>();
-     for(int i = 0;i<n;i++){
-        while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
-            stack.pop();
-        }
-        if(stack.isEmpty()){
-            res[i]= -1;
-        }else{
-            res[i] = stack.peek();
-        }
-        stack.push(i);
-     }
-     return res;
+      }
+     return (max==Integer.MIN_VALUE)?0:max;
+
+
+
+
+
+
+        // using two stack....
+        // int ns[] = findNextSmaller(heights);
+        // int ps[] = findPrevSmaller(heights);
+        // int max = Integer.MIN_VALUE;
+        // for(int i =0;i<heights.length;i++){
+        //     int h = heights[i];
+        //     int w = (ns[i]-ps[i])-1;;
+        //     int area = h*w;
+        //     max= Math.max(max,area);
+        // }
+    // return max;
+    // }
+    // public int[] findNextSmaller(int[] arr){
+    //  int n = arr.length;
+    //  int res[] = new int[n];
+    //  Stack<Integer>stack= new Stack<>();
+    //  for(int i = n-1;i>=0;i--){
+    //     while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+    //         stack.pop();
+    //     }
+    //     if(stack.isEmpty()){
+    //         res[i]= n;
+    //     }else{
+    //         res[i] = stack.peek();
+    //     }
+    //     stack.push(i);
+    //  }
+    //  return res;
+    // }
+
+    // public int[] findPrevSmaller(int[] arr){
+    //  int n = arr.length;
+    //  int res[] = new int[n];
+    //  Stack<Integer>stack= new Stack<>();
+    //  for(int i = 0;i<n;i++){
+    //     while(!stack.isEmpty() && arr[stack.peek()]>=arr[i]){
+    //         stack.pop();
+    //     }
+    //     if(stack.isEmpty()){
+    //         res[i]= -1;
+    //     }else{
+    //         res[i] = stack.peek();
+    //     }
+    //     stack.push(i);
+    //  }
+    //  return res;
     }
 }
