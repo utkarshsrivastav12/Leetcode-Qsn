655e29187772767d5a112b2d2aa8fa7931c7fc1b
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-08-16 06:45:24 +0530

    Time: 4 ms (32.86%), Space: 42.8 MB (54.22%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,44 +1,75 @@
 class Solution {
     public int longestValidParentheses(String s) {
+     //1st method optimize code 
+     Stack<Integer>stack = new Stack<>();
+     int max = 0 ;
+     stack.push(-1);
+     for(int i =0 ;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(ch=='('){
+            stack.push(i);
+        }else{
+            stack.pop();
+            if(stack.isEmpty()){
+                stack.push(i);
+            }
+            int currentIndex = i;
+            int mostRecentInvalid = stack.peek();
+            max=Math.max(max,currentIndex - mostRecentInvalid);
+        }
+     }
+       return max;
+     
+
+
+
+
+
+
+
+
+
+
+        //2nd method Brute force Solution
        //remove all valid parenthesis
-       Stack<Integer>stack = new Stack<>();
-       removeValidParentheses(s,stack) ;
+    //    Stack<Integer>stack = new Stack<>();
+    //    removeValidParentheses(s,stack) ;
        //if stack is emptyh then there were no invid parenthese so return len;
-       if(stack.isEmpty()){
-        return s.length();
-       }
-       ArrayList<Integer>list = new ArrayList<>();
-       formArrayOfInvalidIndexes(list,stack,s);
+    //    if(stack.isEmpty()){
+    //     return s.length();
+    //    }
+    //    ArrayList<Integer>list = new ArrayList<>();
+    //    formArrayOfInvalidIndexes(list,stack,s);
 
        //find max diffrence between two invalid parentheses
-       int max=0;
-       for(int i=1;i<list.size();i++){
-        int prev = list.get(i-1);
-        max=Math.max(max,list.get(i)-prev-1);
-       }
-       return max;
-    }
-    public void removeValidParentheses(String s,Stack<Integer>stack){
-        for(int i = 0; i<s.length();i++){
-            char ch= s.charAt(i);
-            if(ch=='('){
-                stack.push(i);
-            }else{
-                if(stack.isEmpty() || s.charAt(stack.peek())==')'){
-                    stack.push(i);
-                }else{
-                    stack.pop();
-                }
-            }
-        }
-    }
-     public void formArrayOfInvalidIndexes(ArrayList<Integer> list,Stack<Integer> stack,String s){
-        list.add(0,s.length());
+    //    int max=0;
+    //    for(int i=1;i<list.size();i++){
+    //     int prev = list.get(i-1);
+    //     max=Math.max(max,list.get(i)-prev-1);
+    //    }
+    //    return max;
+    // }
+    // public void removeValidParentheses(String s,Stack<Integer>stack){
+    //     for(int i = 0; i<s.length();i++){
+    //         char ch= s.charAt(i);
+    //         if(ch=='('){
+    //             stack.push(i);
+    //         }else{
+    //             if(stack.isEmpty() || s.charAt(stack.peek())==')'){
+    //                 stack.push(i);
+    //             }else{
+    //                 stack.pop();
+    //             }
+    //         }
+    //     }
+    // }
+    //  public void formArrayOfInvalidIndexes(ArrayList<Integer> list,Stack<Integer> stack,String s){
+    //     list.add(0,s.length());
         //now stack will have only the invalid paranthese indexes
-        while(!stack.isEmpty()){
-            list.add(0,stack.pop());
-        }
-        list.add(0,-1);
+        // while(!stack.isEmpty()){
+        //     list.add(0,stack.pop());
+        // }
+        // list.add(0,-1);
      }
    
 }
