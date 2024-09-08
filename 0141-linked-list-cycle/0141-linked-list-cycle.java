d62624c8839cbe87ceb1919c32dd953622af3896
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-09-08 07:22:51 +0530

    Time: 1 ms (19.06%), Space: 43.8 MB (98.32%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,6 +10,29 @@
  * }
  */
 public class Solution {
+    public ListNode firstNodeOfCycle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean found = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                found = true;
+                break;
+        } 
+        }
+        if(!found){
+            return null;
+        }
+        slow = head;
+        while(slow != fast ){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+
+    }
     public boolean hasCycle(ListNode head) {
         ListNode slow = head ;
         ListNode fast = head;
@@ -20,6 +43,9 @@ public class Solution {
                 return true;
             }
         }
+        ListNode startNode = firstNodeOfCycle(head);
+        if(startNode != null)
+         System.out.println("Found starting node -> "+startNode.val);
         return false;
     }
 }
