17c0c13f780ace9e5a489ca80c5ac1f0ca161b2c
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2024-09-10 07:29:25 +0530

    Time: 6 ms (55.19%), Space: 68.8 MB (21.3%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,34 +9,88 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
+    public ListNode reverseList(ListNode head){
         ListNode prev = null;
+        ListNode current = head;
+        while(current != null){
+            ListNode nextNode= current.next;
+            current.next =prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+    public ListNode findMiddle(ListNode head){
         ListNode slow = head;
         ListNode fast = head;
-        ListNode newListHead = null;
         while(fast!=null && fast.next != null){
-            prev = slow;
-            slow = slow .next;
-            fast = fast.next.next;
-            if(newListHead==null){
-                newListHead = new ListNode(prev.val);
-            }else{
-                ListNode node = new ListNode(prev.val);
-                node.next = newListHead;
-                newListHead = node;
+            slow = slow.next;
+            fast =fast.next.next;
+        }
+        return slow;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode middle = findMiddle(head);
+        ListNode tail = reverseList(middle);
+        ListNode temp = head;
+        while(tail!=null){
+            if(tail.val != temp.val){
+                return false;
             }
-        }
-        if(fast != null){//list the odd length skip the middle 
-          slow = slow.next;//skip the middle 
-        }
-        while(slow!= null && newListHead!=null){
-        if(slow.val!=newListHead.val){
-            return false;
-        }
-        slow= slow.next;
-        newListHead = newListHead.next;
+            tail = tail.next;
+            temp = temp.next;
         }
         return true;
-
     }
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         ListNode prev = null;
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         ListNode newListHead = null;
+//         //creating while finding the midle node.O(N/2) this creation of new list as well as find middle 
+//         while(fast!=null && fast.next != null){
+//             prev = slow;
+//             slow = slow .next;
+//             fast = fast.next.next;
+//             if(newListHead==null){
+//                 newListHead = new ListNode(prev.val);
+//             }else{
+//                 ListNode node = new ListNode(prev.val);
+//                 node.next = newListHead;
+//                 newListHead = node;
+//             }
+//         }
+//         if(fast != null){//list the odd length skip the middle 
+//           slow = slow.next;//skip the middle 
+//         }
+//         while(slow!= null && newListHead!=null){
+//         if(slow.val!=newListHead.val){
+//             return false;
+//         }
+//         slow= slow.next;
+//         newListHead = newListHead.next;
+//         }
+//         return true;
+//     }
+// }
