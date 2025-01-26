423a5b360f06e984ff9b83b992c681256f84e848
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-01-26 12:58:16 +0530

    Time: 3 ms (65.24%), Space: 45.2 MB (29.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,48 +1,48 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    for(int i=0;i<nums.length;i++){
-    for(int j = i+1;j<nums.length;j++){
-        if(nums[i]+nums[j]==target){
-            return new int[]{i,j};
-        }
+    // for(int i=0;i<nums.length;i++){
+    // for(int j = i+1;j<nums.length;j++){
+    //     if(nums[i]+nums[j]==target){
+    //         return new int[]{i,j};
+    //     }
 
-    }
-    // return {};
-    }
-    return new int[]{};
-    }}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //    HashMap<Integer,Integer>hm = new HashMap<>();
-    //    for(int i =0 ; i<nums.length;i++){
-    //     if(hm.containsKey(target-nums[i])){
-    //         // int arr[]={hm.get(target-nums[i]),i};
-    //         // if match then return the i and which target meet index return
-    //         return new int[] {hm.get(target-nums[i]),i};
-    //     }
-    //     else{
-    //         hm.put(nums[i],i);
-    //     }
-    //    }
-       
-    //    return null;
     // }
-// }
+    // // return {};
+    // }
+    // return new int[]{};
+    // }}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+       HashMap<Integer,Integer>hm = new HashMap<>();
+       for(int i =0 ; i<nums.length;i++){
+        if(hm.containsKey(target-nums[i])){
+            // int arr[]={hm.get(target-nums[i]),i};
+            // if match then return the i and which target meet index return
+            return new int[] {hm.get(target-nums[i]),i};
+        }
+        else{
+            hm.put(nums[i],i);
+        }
+       }
+       
+       return null;
+    }
+}
