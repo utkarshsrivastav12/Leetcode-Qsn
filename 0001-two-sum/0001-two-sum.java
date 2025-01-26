57c0203c8e27fd2458dd04193cf8f5d75e815358
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-01-26 15:33:25 +0530

    Time: 2 ms (98.91%), Space: 45 MB (44.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -33,15 +33,16 @@ class Solution {
     
        HashMap<Integer,Integer>hm = new HashMap<>();
        for(int i =0 ; i<nums.length;i++){
+        int first=nums[i];
+        int second = target-first;
         if(hm.containsKey(target-nums[i])){
             // int arr[]={hm.get(target-nums[i]),i};
             // if match then return the i and which target meet index return
-            return new int[] {hm.get(target-nums[i]),i};
+            return new int[] {hm.get(second),i};
         }
-        else{
-            hm.put(nums[i],i);
+            hm.put(first,i);
         }
-       }
+       
        
        return null;
     }
