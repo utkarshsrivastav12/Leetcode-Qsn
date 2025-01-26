a2775a750634f56c411dc0af4ee8486540c65cc0
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-01-26 17:33:53 +0530

    Time: 0 ms (100%), Space: 44.3 MB (76.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,29 +10,29 @@
  * }
  */
 public class Solution {
-    public ListNode firstNodeOfCycle(ListNode head){
-        ListNode slow = head;
-        ListNode fast = head;
-        boolean found = false;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast){
-                found = true;
-                break;
-        } 
-        }
-        if(!found){
-            return null;
-        }
-        slow = head;
-        while(slow != fast ){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return slow;
+    // public ListNode firstNodeOfCycle(ListNode head){
+        // ListNode slow = head;
+        // ListNode fast = head;
+    //     boolean found = false;
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //         if(slow == fast){
+    //             found = true;
+    //             break;
+    //     } 
+    //     }
+    //     if(!found){
+    //         return null;
+    //     }
+    //     slow = head;
+    //     while(slow != fast ){
+    //         slow = slow.next;
+    //         fast = fast.next;
+    //     }
+    //     return slow;
 
-    }
+    // }
     public boolean hasCycle(ListNode head) {
         ListNode slow = head ;
         ListNode fast = head;
@@ -43,9 +43,9 @@ public class Solution {
                 return true;
             }
         }
-        ListNode startNode = firstNodeOfCycle(head);
-        if(startNode != null)
-         System.out.println("Found starting node -> "+startNode.val);
+        // ListNode startNode = firstNodeOfCycle(head);
+        // if(startNode != null)
+        //  System.out.println("Found starting node -> "+startNode.val);
         return false;
     }
 }
