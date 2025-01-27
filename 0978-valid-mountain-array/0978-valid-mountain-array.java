7c8121040954e8ef8bf6d08a278d1671488f56be
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-01-27 15:38:01 +0530

    Time: 1 ms (100%), Space: 44.9 MB (98.54%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.java
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -1,28 +1,69 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        int index =0;
-        int n = arr.length;
-        //find a increase seq(find the peek)
-        while(index<n-1){
-            if(arr[index]<arr[index+1]){
-                index++;
-            }else{
-                break; 
-            }
-        }
-        if(index==0 || index==n-1){
-            return false;
-        }
-            while(index<n-1){
-                if(arr[index]>arr[index+1]){
-                  index++;
-                }else{
-                    break;
-                }
-        }
-        if(index==n-1){
-            return true;
-        }
+        int n = arr.length-1;
+    if(arr.length<3){
         return false;
     }
+    int index=0;
+    //find increasing part of the array
+    while(index < n){
+        if(arr[index]<arr[index+1]){
+            index++;
+        }else{
+            break;
+        }
+    }
+    if(index==n || arr[0]>arr[1]){
+        return false;
+    }
+//decrease order
+while(index<n){
+    if(arr[index]>arr[index+1]){
+        index++;
+    }else{
+        return false;
+    }
+} if(index==n){
+    return true;
 }
+return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//         int index =0;
+//         int n = arr.length;
+//         //find a increase seq(find the peek)
+//         while(index<n-1){
+//             if(arr[index]<arr[index+1]){
+//                 index++;
+//             }else{
+//                 break; 
+//             }
+//         }
+//         if(index==0 || index==n-1){
+//             return false;
+//         }
+//         //find a decreasing seq(find the end of the array)
+//             while(index<n-1){
+//                 if(arr[index]>arr[index+1]){
+//                   index++;
+//                 }else{
+//                     break;
+//                 }
+//         }
+//         if(index==n-1){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
