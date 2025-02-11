215bf573e782fbf19f747ba6b81cf19b02e94554
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-02-11 14:49:32 +0530

    Time: 9 ms (19.41%), Space: 42.3 MB (76.96%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -12,31 +12,24 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
         Stack<Character> stack = new Stack<>();
-        int partLength = part.length();
-
+        int pL = part.length();
         for (char c : s.toCharArray()) {
             stack.push(c);
-
-            // Check if the top of the stack matches the part string
-            if (stack.size() >= partLength) {
+            if (stack.size() >= pL) {
                 boolean match = true;
-                for (int i = 0; i < partLength; i++) {
-                    if (stack.get(stack.size() - partLength + i) != part.charAt(i)) {
+                for (int i = 0; i < pL; i++) {
+                    if (stack.get(stack.size() - pL + i) != part.charAt(i)) {
                         match = false;
                         break;
                     }
                 }
-                
-                // If a match is found, pop the last partLength characters
                 if (match) {
-                    for (int i = 0; i < partLength; i++) {
+                    for (int i = 0; i < pL; i++) {
                         stack.pop();
                     }
                 }
             }
         }
-
-        // Build the result from the stack
         StringBuilder result = new StringBuilder();
         for (char c : stack) {
             result.append(c);
