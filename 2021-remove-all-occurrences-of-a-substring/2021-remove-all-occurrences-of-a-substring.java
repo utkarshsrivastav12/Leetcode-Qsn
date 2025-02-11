e13069f15abb4c6bf2f52ce5a80484d77713f353
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-02-11 21:59:44 +0530

    Time: 7 ms (43.37%), Space: 44.3 MB (42.68%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,40 +1,55 @@
-// class Solution {
-//     public String removeOccurrences(String s, String part) {
-//         while (s.contains(part)) {
-//             s = s.replace(part, "");
-//         }
-//         return s;
-
-
-//      import java.util.Stack;
-//     }
-// }
 class Solution {
     public String removeOccurrences(String s, String part) {
-        Stack<Character> stack = new Stack<>();
-        int pL = part.length();
-        for (char c : s.toCharArray()) {
-            stack.push(c);
-            if (stack.size() >= pL) {
-                boolean match = true;
-                for (int i = 0; i < pL; i++) {
-                    if (stack.get(stack.size() - pL + i) != part.charAt(i)) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
-                    for (int i = 0; i < pL; i++) {
-                        stack.pop();
-                    }
-                }
-            }
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
         }
-        StringBuilder result = new StringBuilder();
-        for (char c : stack) {
-            result.append(c);
-        }
+        return s;
 
-        return result.toString();
-    }
+
+    }  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public String removeOccurrences(String s, String part) {
+//         Stack<Character> stack = new Stack<>();
+//         int pL = part.length();
+//         for (char c : s.toCharArray()) {
+//             stack.push(c);
+//             if (stack.size() >= pL) {
+//                 boolean match = true;
+//                 for (int i = 0; i < pL; i++) {
+//                     if (stack.get(stack.size() - pL + i) != part.charAt(i)) {
+//                         match = false;
+//                         break;
+//                     }
+//                 }
+//                 if (match) {
+//                     for (int i = 0; i < pL; i++) {
+//                         stack.pop();
+//                     }
+//                 }
+//             }
+//         }
+//         StringBuilder result = new StringBuilder();
+//         for (char c : stack) {
+//             result.append(c);
+//         }
+
+//         return result.toString();
+//     }
+// }
