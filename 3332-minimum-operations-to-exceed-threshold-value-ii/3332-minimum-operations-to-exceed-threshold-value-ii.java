a8d660db86e64ec0f17e9aaec50e1f5e046847c6
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-02-14 09:39:03 +0530

    Time: 153 ms (31.38%), Space: 70.7 MB (48.12%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,24 +1,81 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        PriorityQueue<Long>pq = new PriorityQueue<>();
-        for(long num : nums){
-            pq.offer(num);
+       
+       PriorityQueue<Long>pq = new PriorityQueue<>();
+       for(long num : nums){
+        pq.offer(num);
+       }
+       int count=0;
+       while(!pq.isEmpty()){
+        if(pq.peek()>=k){
+            return count;
+        }else{
+            count++;
+            long f = pq.poll();
+            long s= pq.poll();
+            long nE = Math.min(f,s)*2+Math.max(f,s);
+            pq.offer(nE);
         }
-        int count=0;
-        while(!pq.isEmpty()){
-            
-            if(pq.peek()>=k){
-                return count;
-            }else{
-                count++;
-                long first  = pq.poll();
-                long second = pq.poll();
-                long nE = Math.min(first,second)*2+ Math.max(first,second);
-                pq.offer(nE);
-
-            }
-           
-        }
-        return count;
+       }
+       return count;
+       
+       
     }
-}
+} 
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+//         PriorityQueue<Long>pq = new PriorityQueue<>();
+//         for(long num : nums){
+//             pq.offer(num);
+//         }
+//         int count=0;
+//         while(!pq.isEmpty()){
+//             if(pq.peek()>=k){
+//                 return count;
+//             }else{
+//                 count++;
+//                 long first  = pq.poll();
+//                 long second = pq.poll();
+//                 long nE = Math.min(first,second)*2+ Math.max(first,second);
+//                 pq.offer(nE);
+
+//             }
+           
+//         }
+//         return count;
+//     }
+// }
