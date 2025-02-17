f793577015661ed7cf290c9d095e002f633c4c25
Utkarsh Srivastav <124474514+utkarshsrivastav12@users.noreply.github.com>
2025-02-17 08:03:13 +0530

    Time: 16 ms (50.17%), Space: 44.6 MB (56.54%) - LeetHub

---
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+         Map<Character, Integer> countMap = new HashMap<>();
+        for (char tile : tiles.toCharArray()) {
+            countMap.put(tile, countMap.getOrDefault(tile, 0) + 1);
+        }
+        return backtrack(countMap);
+    }
+
+    private static int backtrack(Map<Character, Integer> countMap) {
+        int total = 0;
+        for (char tile : countMap.keySet()) {
+            if (countMap.get(tile) > 0) {
+                countMap.put(tile, countMap.get(tile) - 1);
+                total += 1 + backtrack(countMap);
+                countMap.put(tile, countMap.get(tile) + 1); // Backtrack
+            }
+        }
+        return total;
+    }
+}
+
